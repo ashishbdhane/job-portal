83747bf4e817f8d68b1a75a00932d4b6b4298ff9
Ashish <81252055+ashishbdhane@users.noreply.github.com>
2023-03-03 23:13:50 +0530

    Extra Deleted

--- src/Pages/components/Opportunities/CardContent/ItemDetail.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ItemType from './ItemType';
-
-const ItemDetail: React.FC<{ item: ItemType }> = (props) => {
-  return <div>Salary: {props.item.Salary}</div>;
-};
-
-export default ItemDetail;
